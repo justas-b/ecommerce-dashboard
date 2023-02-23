d83edd700f8c639710e6c4a5e916783f4d112219
justas-b <justasb@hotmail.co.uk>
2023-02-23 20:25:58 +0000

    dropping columns that wont be used

--- data-scrub.py
+++ data-scrub.py
@@ -11,4 +11,17 @@ def clean_column_names(df: pd.DataFrame) -> NoReturn:
     df.columns = cleaned_columns
 
 
-clean_column_names(df)
+def drop_unused_columns(columns_to_keep: list, df: pd.DataFrame) -> NoReturn:
+    """Drops columns that will not be needed in the dashboard - need to specify the columns to keep"""
+    to_drop = [col for col in df.columns if col not in columns_to_keep]
+    df.drop(to_drop, axis=1, inplace=True)
+
+
+if __name__ == "__main__":
+    columns_to_keep = ["sale_date", "item_name", "quantity", "price"  "discount_amount", "order_delivery", "item_total", "date_posted", "delivery_zipcode", "delivery_country"]
+    clean_column_names(df)
+    drop_unused_columns(columns_to_keep, df)
+    print(df.info())
+
+#TODO:
+#Convert dates to datetime
