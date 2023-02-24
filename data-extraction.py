8798e47f03dc47c6130bfb7a99da8b5d7011d2db
justas-b <justasb@hotmail.co.uk>
2023-02-24 21:08:44 +0000

    orders per delivery country figure

--- data-extraction.py
+++ data-extraction.py
@@ -1,5 +1,4 @@
 # TODO:
-# orders per delivery country
 # total and average revenue per country
 # distribution of days taken to dispatch orders
 # order distribution across free and paid deliveries
@@ -23,3 +22,17 @@ def total_orders(df: pd.DataFrame) -> int:
 def total_items(df: pd.DataFrame) -> int:
     """Extracts the total number of items ordered"""
     return df["quantity"].sum()
+
+
+def orders_by_country(df: pd.DataFrame) -> px.bar:
+    """Bar plot of the distribution of orders per country"""
+    country_count = df["delivery_country"].value_counts()
+    data = {
+        "Country": reversed(country_count.keys()), 
+        "Orders": reversed(country_count.values)
+        }
+
+    fig = px.bar(data, x="Orders", y="Country", text="Orders")
+    fig.update_traces(textposition="outside")
+
+    return fig
