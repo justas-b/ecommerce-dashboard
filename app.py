b45b4c37189613e01bb8c78fa221f48266b210cc
justas-b <justasb@hotmail.co.uk>
2023-02-26 19:25:38 +0000

    general layout complete with some graphs as placeholders

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dash import Dash, dcc, html
 import dash_bootstrap_components  as dbc
-from data_extraction import total_orders, total_items, total_revenue, average_revenue
+from data_extraction import *
 
 app = Dash(external_stylesheets=[
            dbc.themes.ZEPHYR])
@@ -21,11 +21,21 @@ app.layout = html.Div(
             html.P(className="info_text", children=[
                 f"Average Revenue: £{average_revenue()}"])
         ]),
-        html.Div(id="top_plot_div", children=[
-            html.H1("Top Plot Div")
+        html.Div(className="top_plot_div", id="top_plot_div", children=[
+            html.H2("Orders per Day"),
+            dcc.Graph(figure=orders_per_day())
         ]),
         html.Div(id="bottom_plot_div", children=[
-            html.H1("Bottom Plot Div")
+            html.Div(className="indiv_bottom_graph", children=[
+                html.H2("Orders per Country"),
+                dcc.Graph(figure=orders_by_country())
+                ]),
+            html.Div(className="indiv_bottom_graph", children=[
+                html.H2("Orders per Delivery Type"),
+                dcc.Graph(figure=order_delivery_charge())]),
+            html.Div(className="indiv_bottom_graph", children=[
+                html.H2("Days to Dispatch"),
+                dcc.Graph(figure=days_to_dispatch())])
         ])
     ])
 
