372fafd6a43b8cbbb7332b71e77410485bf6bde5
justas-b <justasb@hotmail.co.uk>
2023-02-26 15:45:40 +0000

    layout for header and info divs

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ app = Dash(external_stylesheets=[
 app.layout = html.Div(
     id="parent_div", children=[
         html.Div(id="header_div", children=[
-            html.H1("Header Div")
-        ]),
+            html.H1("E-commerce Dashboard"),
+            html.P("Dashboard Info")
+        ], style={"text-align": "center", "font-size": "20px"}),
         html.Div(id="info_div", children=[
-            html.H1("Info Div")
+            html.P("Orders: 50", style={"text-align": "center","width": "24%", "display": "inline-block"}),
+            html.P("Items Ordered: 55", style={"text-align": "center", "width": "24%", "display": "inline-block"}),
+            html.P("Revenue: £1000", style={"text-align": "center", "width": "24%", "display": "inline-block"}),
+            html.P("Average Revenue: £30", style={"text-align": "center", "width": "24%", "display": "inline-block"})
         ]),
         html.Div(id="top_plot_div", children=[
             html.H1("Top Plot Div")
