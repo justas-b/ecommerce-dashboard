0ab3784be00aa88ad51631eccb05eb274ce81223
justas-b <justasb@hotmail.co.uk>
2023-12-28 17:04:01 +0000

    Added an apply all transformations method to transformer class

--- src/transformer.py
+++ src/transformer.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class DataTransformer():
     def __init__(self, df: pd.DataFrame) -> None:
-        self.df = df
+        self.df = df.copy()
 
     def clean_column_names(self) -> None:
         """Cleans the column names of the dataframe.
@@ -16,6 +16,12 @@ class DataTransformer():
         """
         self.df["days_to_dispatch"] = (self.df["date_posted"] - self.df["date_paid"]).dt.days
     
+    def apply_transformations(self) -> None:
+        """Applies all transformation methods to the instantiated dataframe.
+        """
+        self.clean_column_names()
+        self.time_to_dispatch()
+
     @staticmethod
     def save_csv(df: pd.DataFrame, filename: str) -> None:
         """Saves the cleaned dataframe to a CSV.
@@ -25,14 +31,3 @@ class DataTransformer():
             filename (str): Filename to save the df as.
         """
         df.to_csv(f"src/data/{filename}.csv", index=False)
-
-
-if __name__ == "__main__":
-    # change to the file name of the dirty CSV file
-    FILE_NAME = "EtsySoldOrderItems2022.csv"
-    df = pd.read_csv(f"src/data/{FILE_NAME}", parse_dates=["Date Paid", "Date Posted"])
-
-    transformer = DataTransformer(df)
-    transformer.clean_column_names()
-    transformer.time_to_dispatch()
-    transformer.save_csv(transformer.df, "cleaned-data")
