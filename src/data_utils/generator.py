a84cf138a94c124857f0afced969490e76583b2c
justas-b <justasb@hotmail.co.uk>
2024-01-12 18:31:05 +0000

    Added more class methods and countries txtfile

--- src/data_utils/generator.py
+++ src/data_utils/generator.py
@@ -1,5 +1,27 @@
+import math
+import datetime
+
+
 class DataGenerator():
     def __init__(self):
         pass
 
+    def get_random_num(self, start: int, end: int) -> int:
+        pass
 
+    def generate_date(self, start: int, end: int, max_days=int|None) -> datetime.datetime:
+        pass
+
+    def generate_country(self) -> str:
+        pass
+    # generate random sale date
+    # generate random quantity
+    # generate random price
+    # generate random paid date
+    # generate random posted date
+    # generate random country
+    # generate random delivery cost
+
+
+if __name__ == "__main__":
+    generator = DataGenerator()
